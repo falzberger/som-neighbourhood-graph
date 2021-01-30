6348e4dbd051728481dd7ad42149779408a76a44
Alexander Falzberger <falzberger.alexander@gmail.com>
2021-01-30 15:02:25 +0100

    Add exercise submission

---
+++ jupyterlab_install.sh
@@ -0,0 +1,4 @@
+# from https://plotly.com/python/getting-started/#jupyterlab-support
+pip install jupyterlab "ipywidgets>=7.5"
+jupyter labextension install jupyterlab-plotly@4.14.3
+jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget@4.14.3
